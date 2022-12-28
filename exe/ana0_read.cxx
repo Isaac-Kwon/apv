4b794d8a78cc4b932d4bce5c05ef71c5a56f9bf3
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2022-12-28 15:15:21 +0900

    modify name of graphset1 in macro ana0_read.cxx

--- exe/ana0_read.cxx
+++ exe/ana0_read.cxx
@@ -1,13 +1,13 @@
 #include "iostream"
 
-#include "APVAnaTaskRun.hpp"
+#include "APVAnaTaskGraphSet1.hpp"
 #include "TString.h"
 #include "TCanvas.h"
 
 int main(){
     // APVAnaRun * bb = new APVAnaRun("../data/kriss_data/apv_2022120725.txt"); //gamma
     // APVAnaRun * bb = new APVAnaRun("../data/kriss_data/apv_2022120803.txt"); //10% sheiding neutron
-    APVAnaTaskRun * bb = new APVAnaTaskRun("../data/kriss_data/apv_2022120725.txt"); //neutron
+    APVAnaTaskGraphSet1 * bb = new APVAnaTaskGraphSet1("../data/kriss_data/apv_2022120725.txt"); //neutron
 
     for(int i=0; i<20; i++){
         bb->GraphOne(i, TString::Format("c1_%d.pdf", i).Data());
