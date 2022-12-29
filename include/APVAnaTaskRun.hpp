5907fcca8703e880c571fe6981903c7c31f1f484
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2022-12-29 13:39:56 +0900

    Organize Runtask

--- include/APVAnaTaskRun.hpp
+++ include/APVAnaTaskRun.hpp
@@ -2,17 +2,37 @@
 #define __APVANARUN__
 
 #include "APVAnaTaskBase.hpp"
+#include "APVStrip.hpp"
 
 class TCanvas;
+class TH1D;
+class TH2D;
 
 class APVAnaTaskRun: public APVAnaTaskBase{
     public:
     APVAnaTaskRun();
     APVAnaTaskRun(std::string filename, std::string name="APVAna0", short Nx=256, short Ny=256);
+    // static void AnalysisOne(APVStrip* sx, APVStrip* sy) ;
     void Analysis() override;
-    void GraphOne(int i, std::string name="c1.pdf");
+    void SetThreshold(short val=100){fThreshold = val;}
+    void SetStartEntry(int val=0){fStartEntry = val;}
+    void SetEndEntry(int val=-1){fEndEntry = val;}
+    TTree * GetTree(){return fTree;}
     protected:
+    TTree * MakeTree();
     private:
+    // TH2D * fhHitmapMean = nullptr; //hitmap [Mean]
+    // TH2D * fhHitmapPeak = nullptr; //hitmap [Peak]
+    // TH1D * fhAmpl       = nullptr; //Amplitudes (peak top)
+    // TH1D * fh3=nullptr;            //Fired Strips
+    TTree * fTree = nullptr;
+    int         fStartEntry = 0;
+    int         fEndEntry = -1;
+    int         fbI;
+    StripResult fbX;
+    StripResult fbY;
+
+    short fThreshold = 100;
 };
 
 #endif
--- src/APVAnaTaskRun.cpp
+++ src/APVAnaTaskRun.cpp
@@ -1,21 +1,27 @@
 #include "APVAnaTaskRun.hpp"
 
 #include "string"
-#include "APVStrips.hpp"
-#include "TAxis.h"
-#include "TCanvas.h"
-#include "TF1.h"
-#include "TGraph.h"
 #include "TTree.h"
-#include "TPaveText.h"
+#include "iostream"
+
+#include "APVStrip.hpp"
 
 APVAnaTaskRun::APVAnaTaskRun():APVAnaTaskBase(){;}
 
 APVAnaTaskRun::APVAnaTaskRun(std::string filename, std::string name, short Nx, short Ny):
 APVAnaTaskBase(filename, name, Nx, Ny){;}
 
-void APVAnaTaskRun::GraphOne(int i, std::string name){
+TTree* APVAnaTaskRun::MakeTree(){
+    fTree = new TTree(TString::Format("Ana%s",GetName().c_str()), TString::Format("Analysis about %s",GetName().c_str()));
+    fTree->Branch("I", &fbI, "I/I");
+    fTree->Branch("X", &fbX, "mean/F:mean_f/F:peakstrip/S:peakvalue/S:sum0/F:sum0_f/F:sum1/F:sum1_f/F:nfired/S");
+    fTree->Branch("Y", &fbY, "mean/F:mean_f/F:peakstrip/S:peakvalue/S:sum0/F:sum0_f/F:sum1/F:sum1_f/F:nfired/S");
+    return fTree;
+}
+
+void APVAnaTaskRun::Analysis(){
     ReadData();
+    if(fTree == nullptr) MakeTree();
 
     TTree * rt = GetRawTree();
     unsigned int * ii = GetIp();
@@ -25,139 +31,22 @@ void APVAnaTaskRun::GraphOne(int i, std::string name){
     short       ny = GetNY();
     unsigned int n = GetN();
 
-    rt->GetEntry(i);
-    
-    APVStrips * sx = new APVStrips(nx, x, "APVStripX");
-    APVStrips * sy = new APVStrips(ny, y, "APVStripY");
-    sx->FindBaseline();
-    sy->FindBaseline();
-    sx->Adjust();
-    sy->Adjust();
-    StripResult resx = sx->Analysis();
-    StripResult resy = sy->Analysis();
+    for(int i=fStartEntry; i<n ; i++){
+        if(fEndEntry > 0 && i>fEndEntry) break;
+        std::cout<<"APVAnaTaskRun::Analysis - i="<<i<<std::endl;
+        rt->GetEntry(i);
+        APVStrip * sx = new APVStrip(nx, x, "APVStripX");
+        APVStrip * sy = new APVStrip(ny, y, "APVStripY");
 
-    TCanvas * c1 = new TCanvas("c1","c1", 1200, 1200);
-    c1->Divide(2,2);
-    c1->cd(1);
-    // std::cout<<"Plot1"<<std::endl;
-    sx->GetGraph()->Draw();
-    sx->GetF1()->Draw("SAME");
-    sx->GetF2()->Draw("SAME");
-    c1->cd(2);
-    // std::cout<<"Plot2"<<std::endl;
-    sy->GetGraph()->Draw();
-    sy->GetF1()->Draw("SAME");
-    sy->GetF2()->Draw("SAME");
-    c1->cd(3);
-    // std::cout<<"Plot3"<<std::endl;
-    sx->GetGraphAdjusted()->Draw();
-    sx->GetGraphAdjusted()->GetYaxis()->SetRangeUser(-400, 5000);
-    sx->GetGraphAdjusted()->GetYaxis()->SetLimits(-400, 5000);
-    TPaveText * pave_x = new TPaveText(0.6, 0.75, 0.95, 0.95, "NB NDC");
-    pave_x->AddText(TString::Format("Mean: %.2f", resx.mean));
-    pave_x->AddText(TString::Format("Stdev: %.2f", resx.stdev));
-    pave_x->AddText(TString::Format("Skewness: %.2f", resx.skewness));
-    pave_x->AddText(TString::Format("[%.1f,%.1f,%.1f]", resx.sum0,resx.sum1,resx.sum2));
-    pave_x->Draw();
-    c1->cd(4);
-    // std::cout<<"Plot4"<<std::endl;
-    sy->GetGraphAdjusted()->Draw();
-    sy->GetGraphAdjusted()->GetYaxis()->SetRangeUser(-400, 5000);
-    sy->GetGraphAdjusted()->GetYaxis()->SetLimits(-400, 5000);
-    TPaveText * pave_y = new TPaveText(0.6, 0.75, 0.95, 0.95, "NB NDC");
-    pave_y->AddText(TString::Format("Mean: %.2f", resy.mean));
-    pave_y->AddText(TString::Format("Stdev: %.2f", resy.stdev));
-    pave_y->AddText(TString::Format("Skewness: %.2f", resy.skewness));
-    pave_y->AddText(TString::Format("[%.1f,%.1f,%.1f]", resy.sum0,resy.sum1,resy.sum2));
-    pave_y->Draw();
-    // std::cout<<"Save"<<std::endl;
+        StripResult rx = sx->Analysis(fThreshold);
+        StripResult ry = sy->Analysis(fThreshold);
+        fbI = i;
+        fbX = rx;
+        fbY = ry;
+        
+        fTree->Fill();
 
-    c1->SaveAs(name.c_str());
-
-    delete sx;
-    delete sy;
-
-    return;
+        delete sx;
+        delete sy;
+    }
 }
-
-void APVAnaTaskRun::Analysis(){
-
-
-}
-
-
-// ProfileResult AnalyzeOne(short* val){
-//     // 0 to 0.5, 0.5 to 1 fit pol1 Linear Robust 0.7
-//     // Get Signal (Integral within linear pol1)
-//     // Get Strip Multiplicity within 2sigma of pol1.
-//     // Fit("pol1", "ROB=0.68")
-
-//     //make profile graph
-//     if(g==nullptr) g = new TGraph();
-//     g->Clear();
-
-//     size_t sz = size();
-//     for(int i=0; i<sz; i++){
-//         g->SetPoint(i, i, at(i));
-//     }
-
-//     //make baseline
-
-//     int half = sz/2;
-//     f1 = new TF1("", "pol1", 0, half-0.5);
-//     f2 = new TF1("", "pol1", half-0.5, sz);
-
-//     g->Fit(f1, "R ROB=0.68 0 Q");
-//     g->Fit(f2, "R ROB=0.68 0 Q");
-
-//     //make signal distribution over baseline
-//     if(h==nullptr) h = new TH1I("", "signal dist", 100, -500, 500);
-//     h->Clear();
-//     for(int i=0; i<sz; i++){
-//         TF1 * f;
-//         if(i<half){f=f1;}else{f=f2;}
-//         h->Fill(at(i)-(f->Eval(i)));
-//     }
-//     fg1 = new TF1("", "gaus", -500, 500);
-//     h->Fit(fg1, "0 Q");
-
-//     //detect outlier channel : 2.5 sigma outlier, from gaus distribution
-//     float cm = fg1->GetParameter("Mean");
-//     float cs = fg1->GetParameter("Sigma");
-
-//     int overlier  = 0;
-//     int underlier = 0;
-//     int oversum   = 0;
-//     int undersum  = 0;
-
-//     for(int i=0; i<sz; i++){
-//         TF1 * f;
-//         if(i<half){f=f1;}else{f=f2;}
-//         if(((at(i)-(f->Eval(i)))-cm) > (4*cs)){
-//             //if over 2.5sigma
-//             overlier ++;
-//             oversum += (at(i)-(f->Eval(i)));
-
-//         }else if(((at(i)-(f->Eval(i)))-cm) < (4*cs)){
-//             //if under -2.5sigma
-//             underlier ++;
-//             undersum += (at(i)-(f->Eval(i)));
-//         }else{
-//             //otherwise
-//         }
-//     }
-
-//     ProfileResult res = {
-//         overlier, // Overlier
-//         oversum, // OverSum
-//         underlier, // Underlier
-//         undersum, // UnderSum
-//         f1->GetParameter("p0"),// F1a
-//         f1->GetParameter("p1"),// F1b
-//         f2->GetParameter("p0"),// F2a
-//         f2->GetParameter("p1") // F2b
-//     };
-
-//     // h->Delete();
-//     return res;
-// }
